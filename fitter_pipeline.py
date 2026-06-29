6ce1e1edb7919f9f1b27372c08505caf8a124574
NikoPicello <niko.picello@gmail.com>
2026-06-29 20:51:11 +0200

    adding base global orient instead of modifying go directly

--- fitter_pipeline.py
+++ fitter_pipeline.py
@@ -325,7 +325,7 @@ if __name__ == '__main__':
         for activity_path in sorted(glob.glob(os.path.join(sid_path, '*'))):
             activity = Path(activity_path).stem
             if 'lego' not in activity: continue
-            for person_id in [0]:
+            for person_id in [0, 1]:
                 trig_path = os.path.join(trig_root, session_id, activity, f"p{person_id}")
                 if not os.path.isdir(os.path.join(trig_path)):
                     continue
